224dd3b83ccefeb1d354d3f721e9741428883442
rahul <129116227+RahulSinghKathayat@users.noreply.github.com>
2025-03-04 16:39:47 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,14 +1,19 @@
+
 import React from "react";
 import { useState } from "react";
 import { Heading } from "../components/Heading";
 import { SubHeading } from "../components/Subheading";
 import { InputBox } from "../components/Inputbox";
 import { Button } from "../components/Button";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 export function Signup(){
     const [ username, setUsername ] = useState("")
     const [ password, setPassword ] = useState("")
+
+    const navigate = useNavigate()
 
     return(
         <div>
@@ -17,12 +22,23 @@ export function Signup(){
                 <SubHeading label={"register yourself first"} />
 
                 <div>
-                    <InputBox label={"Username/Email"} placeholder={"ZmjjKK"}></InputBox>
-                    <InputBox label={"Password"} placeholder={"valorant321"}></InputBox>
+                    <InputBox onChange={(e) => {
+                        setUsername(e.target.value)
+                    }} label={"Username/Email"} placeholder={"ZmjjKK"}></InputBox>
+
+                    <InputBox onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} label={"Password"} placeholder={"valorant321"}></InputBox>
                 </div>
 
                 <div>
-                    <Button label={"Let's Go"}></Button>
+                    <Button onClick={ async() => {
+                        const registerUser = await axios.post("http://localhost:4000/users/signup",{
+                            username,
+                            password
+                        })
+                        navigate("/signin")
+                    }} label={"Let's Go"}></Button>
                 </div>
             </div>
         </div>
